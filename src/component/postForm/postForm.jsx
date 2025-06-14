725806c2da906caa22adb1cefa532d3bd3c70495
madhav9757 <madhavsemwal9@gmail.com>
2025-06-14 16:20:58 +0530

    done all changes

--- src/component/postForm/postForm.jsx
+++ src/component/postForm/postForm.jsx
@@ -103,7 +103,7 @@ export default function PostForm({ post }) {
                 console.error("Failed to update post in Appwrite.");
             }
         } else {
-            if (userData && userData.$id) { 
+            if (userData && userData.$id) {
                 try {
                     const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
 
@@ -113,7 +113,7 @@ export default function PostForm({ post }) {
 
                     const dbPost = await appwriteService.createPost({
                         ...data,
-                        userID: userData.$id, 
+                        userID: userData.$id,
                     });
 
                     if (dbPost) {
